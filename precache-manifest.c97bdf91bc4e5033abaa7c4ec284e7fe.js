d4c6335be2b3954c40adf832d27762e4bf6114ac
Wesley Kok <ryutarro123@gmail.com>
2020-06-19 03:25:11 -0700

    Updates

---
+++ precache-manifest.c97bdf91bc4e5033abaa7c4ec284e7fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3176a66271a907e2637969f1b97074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51c244e54a9734347598",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "375b63e814c95c6a7f47",
+    "url": "/static/css/main.45227209.chunk.css"
+  },
+  {
+    "revision": "51c244e54a9734347598",
+    "url": "/static/js/2.a808d052.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a808d052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375b63e814c95c6a7f47",
+    "url": "/static/js/main.25aa54a9.chunk.js"
+  },
+  {
+    "revision": "0331b19a8794e6988c29",
+    "url": "/static/js/runtime-main.10482c78.js"
+  },
+  {
+    "revision": "57a3c05563284e85ef2fea04bad72d29",
+    "url": "/static/media/movie.57a3c055.png"
+  },
+  {
+    "revision": "409762fe3684f267e39126b80e99c0c3",
+    "url": "/static/media/punchy.409762fe.png"
+  },
+  {
+    "revision": "dc307559d63319f0e31e4ed7c1e85a2f",
+    "url": "/static/media/statue.dc307559.png"
+  },
+  {
+    "revision": "8639fcb93a855fc1eb22af1990e6e8ea",
+    "url": "/static/media/wyvern.8639fcb9.png"
+  }
+]);
